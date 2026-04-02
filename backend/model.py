cdacb35c11bf0c009b7ce0cc918e0860ff0827f9
Haneee20 <haneefa.syednadeem@gmail.com>
2026-04-02 12:08:50 +0530

    Added initial dataset setup in model.py

--- backend/model.py
+++ backend/model.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+
+data = {
+    'temperature': [60, 70, 80, 90, 100],
+    'vibration': [1, 2, 3, 4, 5],
+    'pressure': [30, 35, 40, 45, 50],
+    'failure': [0, 0, 0, 1, 1]
+}
+
+df = pd.DataFrame(data)
+
+print(df)

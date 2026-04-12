1397cdf01f9aaa4c7b519986a1656ee77d81c1e0
Haneee20 <haneefa.syednadeem@gmail.com>
2026-04-12 21:30:55 +0530

    Fixed port binding issue

--- app.py
+++ app.py
@@ -39,5 +39,5 @@ def index():
                            graph=graph_path)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
